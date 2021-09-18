60e70d7cea2943b8c389b89ca62bc541abaf5840
anaroshi <anaroshi@naver.com>
2021-09-18 20:12:41 +0900

    drop tables if exists

--- app/dbDropTable.go
+++ app/dbDropTable.go
@@ -7,26 +7,30 @@ import (
 
 func dbDropTable(db *sql.DB) {
 			
-	statement, err := db.Prepare(`DROP TABLE IF NOT EXISTS apply_subject;`)
+	statement, err := db.Prepare(`DROP TABLE IF EXISTS univ_susi_info_fst;`)
 
 	utils.HandleErr(err, "drop table1")
 	_, err = statement.Exec()
 	utils.HandleErr(err, "drop table11 exec")
 
-	statement, err = db.Prepare(`DROP TABLE IF NOT EXISTS apply_line;`)
+	statement, err = db.Prepare(`DROP TABLE IF EXISTS univ_susi_info_snd;`)
 	utils.HandleErr(err, "drop table2")
 	_, err = statement.Exec()
 	utils.HandleErr(err, "drop table12 exec")
 
-	statement, err = db.Prepare(`DROP TABLE IF NOT EXISTS apply_form;`)
+	statement, err = db.Prepare(`DROP TABLE IF EXISTS univ_susi_info_2021;`)
 	utils.HandleErr(err, "drop table3")
 	_, err = statement.Exec()
 	utils.HandleErr(err, "drop table13 exec")
 
-	statement, err = db.Prepare(`DROP TABLE IF NOT EXISTS apply_dept;`)
+	statement, err = db.Prepare(`DROP TABLE IF EXISTS univ_susi_info_2020;`)
 	utils.HandleErr(err, "drop table4")
 	_, err = statement.Exec()
 	utils.HandleErr(err, "drop table14 exec")
 
+	statement, err = db.Prepare(`DROP TABLE IF EXISTS univ_susi_info_2019;`)
+	utils.HandleErr(err, "drop table5")
+	_, err = statement.Exec()
+	utils.HandleErr(err, "drop table15 exec")
 
 }
